a69655999469e2eff7d73c38beb6811a3522fc31
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2026-09-15 19:12:41 -0300

    fix: update main class assignment for dynamic styling based on route

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import NotFound from './pages/NotFound'
+import { useLocation } from 'react-router'
 
 export default function App() {
+  const location = useLocation()
+  const mainClass = location.pathname === '/' ? 'main-home' : 'main-general'
 
   return (
     <>
       <Header />
-      <main>
+      <main className={mainClass}>
         <Routes>
           <Route path = '/' element={<Home />}/>
           <Route path = '/login' element={<Login />}/>
