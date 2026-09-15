2885d82420ca3bfecd386677c4d805d80a3100ad
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2026-09-15 19:13:20 -0300

    fix: enhance Login component with new styles and layout adjustments

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,26 @@
+import styles from './Login.module.css'
+import { Link } from 'react-router'
+
 export default function Login() {
     
     return (
         <>
-            <div className="card">
-                <h2>You need an account to post a movie.</h2>
-                <form action="">
+            <div className={styles.card}>
+                <form action="" className={styles.loginForm}>
+                    <h1>Login</h1>
                     <label htmlFor="username">Username</label>
                     <input type="text" id="username" name="username" placeholder=""/>
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" name="password"/>
+                    <div className={styles.forget}>
+                        <input type="checkbox" id='checkbox' name='checkbox'/>
+                        <label htmlFor="checkbox">Remember Me</label>
+                        <Link to={'/'}>Forget Password</Link>
+                    </div>
+                    <button>Log in</button>
+                    <div className={styles.register}>
+                        <p>Don't have an account? <Link to={'/'}>Register</Link></p>
+                    </div>
                 </form>
             </div>
         </>
