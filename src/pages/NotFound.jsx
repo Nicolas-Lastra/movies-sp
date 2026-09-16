c6d61136e1e0d2d1a06d514906356a9f1afce8b5
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2026-09-16 20:24:57 -0300

    fix: add ref prop to NotFound component for improved functionality

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,9 @@
-export default function NotFound() {
+export default function NotFound({ ref }) {
     
     return (
-        <>
-        <h1>Not found</h1>
-        <p>The provided URL does not exists.</p>
-        </>
+        <section ref={ref}>
+            <h1>Not found</h1>
+            <p>The provided URL does not exists.</p>
+        </section>
     )
 }
